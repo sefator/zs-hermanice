c5bab604ead99aa561be3b628035f2a10ccf5bcf
Jiri Gabrys <jiri.gabrys@gmail.com>
2026-03-07 10:19:33 +0100

    favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,31 @@ export const metadata: Metadata = {
   title: "ZŠ Heřmanice",
   description:
     "Komunitní základní škola Heřmanice – moderní výuka, otevřený přístup a propojení s přírodou.",
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon-16x16.png',
+      },
+      {
+        rel: 'mask-icon',
+        url: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
